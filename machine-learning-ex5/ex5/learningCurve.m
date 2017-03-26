19e61386aac98f95ca641685122c5440cf059a31
Archie <xpopchi@gmail.com>
2017-03-26 21:22:55 +0800

    added ex5.

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -53,7 +53,15 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
+for i = 1:m
+	X_compute = X(1:i, :);
+	y_compute = y(1:i);
 
+	theta = trainLinearReg(X_compute, y_compute, lambda);
+
+	error_train(i) = linearRegCostFunction(X_compute, y_compute, theta, 0);
+	error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
+end
 
 
 
--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -18,10 +18,14 @@ grad = zeros(size(theta));
 %
 %               You should set J to the cost and grad to the gradient.
 %
+h = X * theta;
+regularization = lambda/(2*m)*sum(theta(2:end).^2);
+J = 1/(2*m)*sum((h - y).^2) + regularization;
 
+regularization_d = lambda/m .* theta;
+regularization_d(1) = 0;
 
-
-
+grad = 1/m .* X' * (h - y) + regularization_d;
 
 
 
